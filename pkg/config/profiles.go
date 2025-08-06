94cce682eb54e6ccfc059886df7385a078b3bbeb
Sascha L. Teichmann <sascha.teichmann@intevation.de>
2025-08-06 13:35:08 +0200

    check profile definitions

--- pkg/config/profiles.go
+++ pkg/config/profiles.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"fmt"
+	"strings"
 )
 
 // Profiles are the profiles served by this contravider.
@@ -9,14 +10,11 @@ type Profiles map[string][]string
 
 // UnmarshalTOML implements [toml.Unmarshaler].
 func (p *Profiles) UnmarshalTOML(data any) error {
-
 	m, ok := data.(map[string]any)
 	if !ok {
 		return fmt.Errorf("unexpected type %T", data)
 	}
-
 	pr := make(Profiles, len(m))
-
 	for k, v := range m {
 		l, ok := v.([]any)
 		if !ok {
@@ -39,7 +37,43 @@ func (p *Profiles) UnmarshalTOML(data any) error {
 	return nil
 }
 
+// check checks for cyclic and undefined definitions.
 func (p Profiles) check() error {
-	// TODO: check if all are defined, cyclic checks.
+	checkProfile := func(name string, branches []string) error {
+		seen := map[string]bool{name: true}
+		var resolve func(string) error
+		resolve = func(def string) error {
+			if !strings.HasPrefix(def, "#") {
+				return nil
+			}
+			// its a reference.
+			def = def[1:]
+			if seen[def] {
+				return fmt.Errorf("self recursive definition %q", def)
+			}
+			seen[def] = true
+			branches, ok := p[def]
+			if !ok {
+				return fmt.Errorf("undefined defintion %q", def)
+			}
+			for _, branch := range branches {
+				if err := resolve(branch); err != nil {
+					return err
+				}
+			}
+			return nil
+		}
+		for _, branch := range branches {
+			if err := resolve(branch); err != nil {
+				return err
+			}
+		}
+		return nil
+	}
+	for name, branches := range p {
+		if err := checkProfile(name, branches); err != nil {
+			return err
+		}
+	}
 	return nil
 }
