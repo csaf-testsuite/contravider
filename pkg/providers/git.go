89fb39980003977154a086767ffd114c7b5270c4
Sascha L, Teichmann <sascha.teichmann@intevation.de>
2025-08-08 08:33:57 +0200

    do initial worktree add of branches

--- pkg/providers/git.go
+++ pkg/providers/git.go
@@ -19,27 +19,54 @@ import (
 	"path/filepath"
 )
 
-func initialCheckout(url, directory string, branches []string) error {
-	create := false
-	cloneDir := filepath.Join(directory, "checkout")
+func initialCheckout(url, workdir string, branches []string) error {
+
+	absWorkDir, err := filepath.Abs(workdir)
+	if err != nil {
+		return fmt.Errorf("abs failed: %w", err)
+	}
+	workdir = absWorkDir
+	cloneDir := filepath.Join(workdir, "checkout")
+
+	//TODO: Create workdir
+
+	clone := false
 	if _, err := os.Stat(cloneDir); err != nil {
 		if !errors.Is(err, os.ErrNotExist) {
 			return err
 		}
-		create = true
+		clone = true
 	}
 
-	if create {
+	if clone {
 		cmd := exec.Command("git", "clone", url, cloneDir)
 		output, err := cmd.CombinedOutput()
 		if err != nil {
 			slog.Error("clone failed", "msg", output)
 			return fmt.Errorf("clone failed: %w", err)
 		}
+	} else {
+		// TODO: git pull
 	}
 
-	//TODO: Checkout the branches.
-	_ = branches
+	for _, branch := range branches {
+		branchDir := filepath.Join(workdir, branch)
+		if _, err := os.Stat(branchDir); err != nil {
+			if !errors.Is(err, os.ErrNotExist) {
+				return err
+			}
+			// Create
+			cmd := exec.Command("git", "worktree", "add", branchDir, branch)
+			cmd.Dir = cloneDir
+			output, err := cmd.CombinedOutput()
+			if err != nil {
+				slog.Error("worktree add failed", "msg", output)
+				return fmt.Errorf("worktree add failed: %w", err)
+			}
+		} else {
+			//TODO: git pull
+		}
+	}
 
 	return nil
 }
