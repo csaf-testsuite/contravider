b429ab3fe509b51924a713f2c42a980b585c0bdb
Sascha L. Teichmann <sascha.teichmann@intevation.de>
2025-08-08 12:03:03 +0200

    Create workdir. ignore main. Only update existing branches

--- pkg/providers/git.go
+++ pkg/providers/git.go
@@ -26,9 +26,16 @@ func initialCheckout(url, workdir string, branches []string) error {
 		return fmt.Errorf("abs failed: %w", err)
 	}
 	workdir = absWorkDir
-	cloneDir := filepath.Join(workdir, "checkout")
+	cloneDir := filepath.Join(workdir, "main")
 
-	//TODO: Create workdir
+	if _, err := os.Stat(workdir); err != nil {
+		if !errors.Is(err, os.ErrNotExist) {
+			return err
+		}
+		if err := os.MkdirAll(workdir, 0777); err != nil {
+			return fmt.Errorf("creating workdir failed: %w", err)
+		}
+	}
 
 	clone := false
 	if _, err := os.Stat(cloneDir); err != nil {
@@ -38,18 +45,28 @@ func initialCheckout(url, workdir string, branches []string) error {
 		clone = true
 	}
 
-	if clone {
+	if clone { // Fresh checkout
 		cmd := exec.Command("git", "clone", url, cloneDir)
 		output, err := cmd.CombinedOutput()
 		if err != nil {
 			slog.Error("clone failed", "msg", output)
 			return fmt.Errorf("clone failed: %w", err)
 		}
-	} else {
-		// TODO: git pull
+	} else { // Only update
+		cmd := exec.Command("git", "pull")
+		cmd.Dir = cloneDir
+		output, err := cmd.CombinedOutput()
+		if err != nil {
+			slog.Error("git pull failed", "msg", output, "err", err)
+			return fmt.Errorf("git pull failed: %w", err)
+		}
 	}
 
 	for _, branch := range branches {
+		if branch == "main" {
+			// Ignore main as it already there.
+			continue
+		}
 		branchDir := filepath.Join(workdir, branch)
 		if _, err := os.Stat(branchDir); err != nil {
 			if !errors.Is(err, os.ErrNotExist) {
@@ -60,11 +77,17 @@ func initialCheckout(url, workdir string, branches []string) error {
 			cmd.Dir = cloneDir
 			output, err := cmd.CombinedOutput()
 			if err != nil {
-				slog.Error("worktree add failed", "msg", output)
+				slog.Error("worktree add failed", "msg", output, "err", err)
 				return fmt.Errorf("worktree add failed: %w", err)
 			}
-		} else {
-			//TODO: git pull
+		} else { // Only update
+			cmd := exec.Command("git", "pull")
+			cmd.Dir = branchDir
+			output, err := cmd.CombinedOutput()
+			if err != nil {
+				slog.Error("git pull failed", "msg", output, "err", err)
+				return fmt.Errorf("git pull failed: %w", err)
+			}
 		}
 	}
 
