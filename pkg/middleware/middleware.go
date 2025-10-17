f527180fd54a2f63966bffa3c8301ea8235ecd57
Sascha L. Teichmann <sascha.teichmann@intevation.de>
2025-10-17 11:36:03 +0200

    Make linter happy.

--- pkg/middleware/middleware.go
+++ pkg/middleware/middleware.go
@@ -8,8 +8,7 @@
 // * 2025 Intevation GmbH <https://intevation.de>
 // * 2025 Fraunhofer Institute for Applied an Integrated Security (AISEC) <https://aisec.fraunhofer.de>
 
-// Package web contains the web controller logic.
-
+// Package middleware contains the semi-static file serving of the instantiated profiles.
 package middleware
 
 import (
